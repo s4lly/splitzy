fe8243edc58acd23ca6f2e8513f6dda6e25c4a82
s4lly <3497128+s4lly@users.noreply.github.com>
2025-08-30 18:22:40 -0700

    update script with better config according to docs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: "public",
-  envPrefix: ["VITE_", "REACT_APP_"],
+  publicDir: 'public',
+  envPrefix: ['VITE_', 'REACT_APP_'],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
