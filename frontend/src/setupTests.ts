c1c97d59f09bb835e8f92c3e13f03c9a0bde7576
s4lly <3497128+s4lly@users.noreply.github.com>
2025-11-28 19:27:57 -0800

    ProgressEvent mock ignores constructor parameters.

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -31,12 +31,16 @@ global.ResizeObserver = class ResizeObserver {
 
 // Mock ProgressEvent for MSW/XMLHttpRequest
 global.ProgressEvent = class ProgressEvent extends Event {
+  lengthComputable: boolean;
+  loaded: number;
+  total: number;
+  
   constructor(type: string, eventInitDict?: ProgressEventInit) {
     super(type, eventInitDict);
+    this.lengthComputable = eventInitDict?.lengthComputable ?? false;
+    this.loaded = eventInitDict?.loaded ?? 0;
+    this.total = eventInitDict?.total ?? 0;
   }
-  lengthComputable = false;
-  loaded = 0;
-  total = 0;
 } as any;
 
 // Mock matchMedia
